2fae036bd78206dd6f04b9cf2038aeaaab44bf7a
Samantha <44854641+soulg3m@users.noreply.github.com>
2018-11-30 15:27:18 -0500

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -13,6 +9,15 @@ and open the template in the editor.
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
         <link href="css/reset.css" rel="stylesheet" type="text/css"/>
         <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130177783-1"></script>
+            <script>
+                 window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                       gtag('js', new Date());
+                       gtag('config', 'UA-130177783-1');
+            </script>
     </head>
     <body>
         <div class="page">
@@ -36,8 +41,30 @@ and open the template in the editor.
             </header>
             <main>
                 <div class="maincontainer">
-                    <div class="maincontent">
-                        Stuff goes here.
+                    <div class="maincontent_np_white">
+                        <div class="boxcontainer">
+                            <div class="onenested_box">
+                                <div class="onenested_box">
+                                    <h2>Subscribe Today!</h2><br>
+                                    <br>
+                                    <p>Subscribe now and start enjoying the season.</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <br>
+                            <form>
+                                <label for="fname">First Name</label>
+                                <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+                                <label for="lname">Last Name</label>
+                                <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+                                <label for="email">Email</label>
+                                <input type="text" id="email" name="email" placeholder="Your email address..">
+
+                                <input type="submit" value="Subscribe!">
+                            </form>
+                        </div>
                     </div>
                 </div>
             </main>
