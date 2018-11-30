c05e366550c799af83a809c483671b31652f49c0
Samantha <44854641+soulg3m@users.noreply.github.com>
2018-11-30 15:26:08 -0500

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -14,6 +10,15 @@ and open the template in the editor.
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
         <link href="css/reset.css" rel="stylesheet" type="text/css"/>
         <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+    
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130177783-1"></script>
+            <script>
+                 window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                       gtag('js', new Date());
+                       gtag('config', 'UA-130177783-1');
+            </script>    
     </head>
     <body>
         <div class="page">
@@ -37,8 +42,19 @@ and open the template in the editor.
             </header>
             <main>
                 <div class="maincontainer">
-                    <div class="maincontent">
-                        Embedded Feedback here.
+                    <div class="maincontent_np_white">
+                        <div class="boxcontainer">
+                            <div class="onenested_box">
+                                <div class="onenested_box">
+                                    <h2>We Love Feedback!</h2><br>
+                                    <br>
+                                    <p>Help us improve our product with your input so we can continue serving our subscribers in the best possible way.</p>
+                                </div>
+                            </div>
+                            <hr>
+                            <br>
+                            <!-- EMBED FEEDBACK FORM FROM GOOGLE HERE!-->
+                        </div>
                     </div>
                 </div>
             </main>
