507d6e7db6ad94fd4f451ba4b0c18be8040beb77
Samantha <44854641+soulg3m@users.noreply.github.com>
2018-11-30 15:26:39 -0500

    Update holiday.php

--- holiday.php
+++ holiday.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -14,6 +10,15 @@ and open the template in the editor.
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
         <link href="css/reset.css" rel="stylesheet" type="text/css"/>
         <link href="css/responsive.css" rel="stylesheet" type="text/css"/>
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130177783-1"></script>
+            <script>
+                 window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                       gtag('js', new Date());
+                       gtag('config', 'UA-130177783-1');
+            </script>
     </head>
     <body>
         <div class="page">
@@ -37,8 +42,44 @@ and open the template in the editor.
             </header>
             <main>
                 <div class="maincontainer">
-                    <div class="maincontent">
-                        Holiday Boxes Here.
+                    <div class="maincontent_np_white">
+                        <div class="boxcontainer">
+                            <div class="onenested_box">
+                                <div class="onenested_box">
+                                    <h2>Holiday Boxes</h2><br>
+                                    <br>
+                                    <p>Purchase our Holiday Boxes for a discounted price when you <b>subscribe today!</b></p>
+                                </div>
+                            </div>
+                            <hr>
+                            <br>
+                            <div class="flex_imgs">
+                                <div class="fadeImg">
+                                    <img src="img/pexels-photo-712318.jpeg" alt="christmas decor" style="width:100%"/>
+                                    <div class="FIcontainer">
+                                        <p class="boxtext">The Christmas 2018 Box</p>
+                                        <p>Subscriber special: </p><p class="boxprice">$19.99</p>
+                                        <p>Or purchase individually for</p><p class="boxprice">$29.99</p>
+                                    </div>
+                                </div>
+                                <div class="fadeImg">
+                                    <img src="img/food-holiday-love-holidays.jpg" alt="thanksgiving decor" style="width:100%"/>
+                                    <div class="FIcontainer">
+                                        <p class="boxtext">The Thanksgiving 2018 Box</p>
+                                        <p>Subscriber special: </p><p class="boxprice">$19.99</p>
+                                        <p>Or purchase individually for</p><p class="boxprice">$29.99</p>
+                                    </div>
+                                </div>
+                                <div class="fadeImg">
+                                    <img src="img/pexels-photo-1449058.jpeg" alt="halloween decor" style="width:100%"/>
+                                    <div class="FIcontainer">
+                                        <p class="boxtext">The Halloween 2018 Box</p>
+                                        <p>Subscriber special: </p><p class="boxprice">$19.99</p>
+                                        <p>Or purchase individually for</p><p class="boxprice">$29.99</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
@@ -50,3 +91,4 @@ and open the template in the editor.
         </div>
     </body>
 </html>
+
